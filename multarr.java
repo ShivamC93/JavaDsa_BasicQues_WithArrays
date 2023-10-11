1fdc914bb34c7d8c5c15a8370cad2fb415781fa7
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2023-10-11 19:44:27 +0530

    Add files via upload

---
+++ multarr.java
@@ -0,0 +1,13 @@
+public class multarr {
+    public static void main(String[] args) {
+//        int[][] arr1 = new int[3][3];
+
+        int[][] arr1 = {{1,2,3},{4,5,6},{7,8,9}};  
+
+        System.out.println(arr1[0][0]);
+        System.out.println(arr1[0][1]);
+        System.out.println(arr1[0][2]);
+     
+
+    }
+}
