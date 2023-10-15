558a96562cc2d526b4a5578a05c7cf770c4dd94a
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2023-10-15 12:57:40 +0530

    Add files via upload

---
+++ arrayex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class arrayex{
+    public static void main(String[] args) {
+        int[] age = new int[3];
+        System.out.println(age.length);
+        age[0]= 5;
+        age[1]=12;
+        age[2]=15;
+        System.out.println(age[0]);
+
+    }
+}
